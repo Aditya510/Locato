37f94d1c752de6aa614017a39ad18d2549ba998a
Aditya <aditya510@gmail.com>
2019-02-26 12:15:40 +0530

    Clean up the frontend directory

--- Frontend/javascript/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
-var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-document.getElementById('name').innerHTML = obj.name;
--- Frontend/jquery/js/scripts.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("#trigger").click(function() {
-    $("#demo").html("Hello, World!");
-    });
-});
